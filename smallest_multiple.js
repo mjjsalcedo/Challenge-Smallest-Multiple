9f910bc366bf722014e43a1dc606644b4d280a9e
Mariel Salcedo <Mariel Salcedo>
2017-06-26 09:23:49 -1000

    starting out skeleton

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 /**
  * Build a function that finds the smallest positive number that is evenly
  * divisible by all of the numbers starting from 1 up to the value passed into your function.
@@ -9,6 +11,19 @@
 module.exports = function( ceiling ) {
   // do work here
 
+    var factor_0 = [];
+    var smallestPositive = 0;
 
-  return 0;
+      for (var i = 1; i <= ceiling; i++) {
+        for (var j = 1; j <= ceiling; j++){
+        let combo = i * j;
+          if(combo % 2 === 0){
+              factor_0.push(combo);
+              smallestPositive = Math.max(factor_0);
+          }
+        }
+     }
+
+     return smallestPositive;
+
 };
